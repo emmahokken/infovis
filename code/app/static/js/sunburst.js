8928d5d6c330c5704d3bf9833555c275eb175ea4
emmahokken <emmahokken@gmail.com>
2020-03-12 15:20:58 +0100

    sunburst fix

--- code/app/static/js/sunburst.js
+++ code/app/static/js/sunburst.js
@@ -58,7 +58,6 @@ function getData() {
     return data_dict;
 }
 
-<<<<<<< HEAD
 function makeSunburst() {
 
     var data = getData();
@@ -175,115 +174,6 @@ function makeSunburst() {
             .attrTween("d", d => () => arc(d.current));
 
     }
-
-=======
-color = d3v5.scaleOrdinal(d3v5.quantize(d3v5.interpolateRainbow, data.children.length + 1))
-format = d3v5.format(",d")
-width = 932
-radius = width / 6
-x_start = 100
-y_start = 100
-
-arc = d3v5.arc()
-    .startAngle(d => d.x0)
-    .endAngle(d => d.x1)
-    .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
-    .padRadius(radius * 1.5)
-    .innerRadius(d => d.y0 * radius)
-    .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
-    // .attr("transform", "translate(500, 400)")
-    // .attr("cx", "10")
-
-
-const root = partition(data);
-
-root.each(d => d.current = d);
-
-const svg = d3v5.select(".sunburst")
-    .attr("viewBox", [x_start, y_start, width, width])
-    .style("font", "10px sans-serif");
-
-const g = svg.append("g")
-    .attr("transform", `translate(${(width / 2)+x_start},${(width / 2)+y_start})`);
-
-
-const path = g.append("g")
-  .selectAll("path")
-  .data(root.descendants().slice(1))
-  .join("path")
-    .attr("fill", d => { while (d.depth > 1) d = d.parent; return d.data.name; })
-    .attr("fill", d => { while (d.depth > 2) d = d.parent; return d.data.name; })
-    .attr("d", d => arc(d.current));
-
-path.filter(d => d.children)
-    .style("cursor", "pointer")
-    .on("click", clicked)
-
-path.append("title")
-    .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
-//
-// const label = g.append("g")
-//     .attr("pointer-events", "none")
-//     .attr("text-anchor", "middle")
-//     .style("user-select", "none")
-//   .selectAll("text")
-//   .data(root.descendants().slice(1))
-//   .join("text")
-//     .attr("dy", "0.35em")
-//     //.attr("fill-opacity", d => +labelVisible(d.current))
-//     .attr("transform", d => labelTransform(d.current))
-//     .text(d => d.data.name);
-
-const parent = g.append("circle")
-    .style("cursor", "pointer")
-    .datum(root)
-    .attr("r", radius)
-    .attr("fill", "none")
-    .attr("pointer-events", "all")
-    .on("click", clicked);
-
-/*
-
-    x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-    x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-    y0: Math.max(0, d.y0 - p.depth),
-    y1: Math.max(0, d.y1 - p.depth)
-
-*/
-
-
-
-function clicked(p) {
-  parent.datum(p.parent || root);
-
-  console.log(p)
-  root.each(d => d.target = {
-    x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-    x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-    y0: Math.max(0, d.y0),
-    y1: Math.max(0, d.y1)
-  });
-
-  const t = g.transition().duration(750);
-
-  // Transition the data on all arcs, even the ones that aren’t visible,
-  // so that if this transition is interrupted, entering arcs will start
-  // the next transition from the desired position.
-  path.transition(t)
-    .tween("data", d => {
-    if (d.depth == 2) { // only update outer ring
-        const i = d3v5.interpolate(d.current, d.target);
-        return t => d.current = i(t);
-    }})
-
-    .filter(function(d) {
-        return +this.getAttribute("fill-opacity") || arcVisible(d.target);
-    })
-        .attr("fill-opacity", d => 100)
-        .attrTween("d", d => () => arc(d.current));
->>>>>>> e19e4a25f3d2d352629966c690e64efd2dfa323b
-
-
 
 }
 function arcVisible(d) {
